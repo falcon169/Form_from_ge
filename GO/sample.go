01cb576e14db2b5e58d51b4f9b5f4197bd3afb55
Lavan Kumar Peechara <lavan.peechara@gmail.com>
2024-07-10 12:44:15 +0000

    go routine works with golang installed

--- GO/sample.go
+++ GO/sample.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("https://go.dev")
+	//resp, err := http.Get("https://go.dev")
+	resp, err := http.Get("https://httpbin.org/get")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	start := time.Now()
 	//We Read the response body on the line below.
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	log.Println("Everything:", time.Since(start))
+	fmt.Printf("client: status code: %d\n", resp.StatusCode)
 	//Convert the body to type string
 	sb := string(body)
 	log.Printf(sb)
